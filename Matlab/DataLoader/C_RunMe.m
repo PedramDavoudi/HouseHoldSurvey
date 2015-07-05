fd0e37d77838fa8b086c3740ecc6ce0c7b73113f
پدرام داودی <pedram.davoudi@gmail.com>
2015-07-06 03:05:10 +0430

    Update C_RunMe.m

--- Matlab/DataLoader/C_RunMe.m
+++ Matlab/DataLoader/C_RunMe.m
@@ -1,30 +1,28 @@
-%function RunMe(StartYear,EndYear)
-% Runner
+function RunMe(StartYear,EndYear)
+% extract defined personal information and cost
+% output is an mat file
 tic;
-%{
+if nargin==1 
+    EndYear=StartYear;
+end
+%{ 
+% uncomment to see the browser and choose the access file
 [FileName,PathName,~] = uigetfile('*.*');% get file location 
 Fname=[PathName FileName];
 Y=str2double(FileName(1:2));
 %}
-%%{ 
+%% 
 %the below code is ok just in my pc
-Y=80;
-[~, Fname]=init0(Y,0); % initial Value
-
-%}
-%{ 
-%for iteration
-if nargin==1 
-    EndYear=StartYear;
-end
+% you have to change the address in init0.m
 for Y=StartYear:EndYear
-%}
-%TT=vertcat(R,U);
+[~, Fname]=init0(Y,0); % initial Value
+% augment Rural and urban datasets
 TT=vertcat(join(codec(Y,0,Fname), codep(Y,0,Fname),'key','Address','Type','outer','MergeKeys',true),join(codec(Y,1,Fname), codep(Y,1,Fname),'key','Address','Type','outer','MergeKeys',true));
 eval(['T' num2str(Y) '=TT;']);
 %export(TT,'XLSfile',['T' num2str(Y)]);
+save(['T' num2str(Y)],['\out\T' num2str(Y)]);
 clear TT;
-%end
+end
 a=toc;
 clc;
 disp(['All Done in ' num2str(fix(a/60)) ':' num2str(a-60*fix(a/60))]);
