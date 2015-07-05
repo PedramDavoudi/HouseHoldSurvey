c820a509d499e9fcc5d8440ebf671ad83fc61fc6
پدرام داودی <pedram.davoudi@gmail.com>
2015-07-06 03:11:23 +0430

    Update init0.m

--- Matlab/DataLoader/init0.m
+++ Matlab/DataLoader/init0.m
@@ -1,12 +1,16 @@
 function [Rg  Fname]=init0(Y,r)
 % this function return the address of file
+% do not hesitate to chnage the addreess
+% some a few changes is needed if you have diffrent data sources
+Adres = 'E:\Data\HouseHoldExpenditure\Access\';
+
 if r==0
     Rg='R';
 else
     Rg='U';
 end
 %%
-Adres = 'E:\Data\HouseHoldExpenditure\Access\';
+
 if Y > 88 
     Fname = [num2str(Y) '.accdb'];% '"89\89.accdb"
 else%if Y > 82 
