b25c786696c65d415d5a993e7db47198615a6e2b
Majid Einian <m.einian@einian-PC.mbri-1.org>
2015-07-22 15:24:09 +0430

    small modifications in Settings

--- R/modifySettings.R
+++ R/modifySettings.R
@@ -1,11 +1,11 @@
 library(yaml)
 Settings <- list(HIESPath       ="D:/MajidFiles/HEIS/",
                  HIESRARPath    ="D:/MajidFiles/HEIS/RARs/",
-                 HIESRAWPath    ="D:/MajidFiles/HEIS/RAW/",
+                 HIESRAWPath    ="D:/MajidFiles/HEIS/DataRAW/",
                  HIESSummaryPath="D:/MajidFiles/HEIS/Summary/",
-                 startyear=70,
+                 startyear=63,
                  endyear=92,
-                 os="windows",
+                 OS="windows",
                  RawDataWebAddress="http://www.amar.org.ir/Portals/0/amarmozuii/hazinedaramad/"
 )
 write(as.yaml(Settings),file = "Settings.yaml")
