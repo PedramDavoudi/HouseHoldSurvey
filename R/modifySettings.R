ec0d0713a30293849ce32d3d8974e25cfbe739b7
Majid Einian <einian85@gmail.com>
2015-11-30 16:23:54 +0330

    Updated setting to support recent years changes in data

--- R/modifySettings.R
+++ R/modifySettings.R
@@ -1,12 +1,19 @@
 library(yaml)
-Settings <- list(HEISPath       ="D:/HEIS/",
-                 HEISRARPath    ="D:/HEIS/RARs/",
-                 HEISRAWPath    ="D:/HEIS/DataRAW/",
-                 HEISSummaryPath="D:/HEIS/Summary/",
-                 HEISDataPath   ="D:/HEIS/DataProcessed/",
+HEISPath           = "D:/HEIS/"
+HEISCompressedPath = paste0(HEISPath,"DataCompressed/")
+HEISRawPath        = paste0(HEISPath,"DataRAW/")
+HEISDataPath       = paste0(HEISPath,"DataProcessed/")
+
+
+Settings <- list(HEISPath          = HEISPath,
+                 HEISCompressedPath=HEISCompressedPath,
+                 HEISRawPath       =HEISRawPath,
+                 HEISDataPath      =HEISDataPath,
                  startyear=63,
-                 endyear=92,
+                 endyear=93,
                  OS="windows",
-                 RawDataWebAddress="http://www.amar.org.ir/Portals/0/amarmozuii/hazinedaramad/"
+                 RawDataWebAddress="http://www.amar.org.ir/Portals/0/amarmozuii/hazinedaramad/",
+                 CompressedFileNamesFile="../Data/CompressedFileNames.csv",
+                 weightsFile="../Data/AllWeights.rda"
 )
 write(as.yaml(Settings),file = "Settings.yaml")
